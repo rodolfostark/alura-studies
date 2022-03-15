7398828e7b31f61170c864e0b954a7dd502cafa4
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2022-03-15 12:06:16 -0300

    feat: adding onChange to the components

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -7,18 +7,38 @@ class Form extends React.Component {
         tarefa: '',
         tempo: '00:00',
     }
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        console.log(this.state);
+    }
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">Adicione um novo estudo</label>
-                    <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
+                    <input 
+                        type="text"
+                        name="tarefa"
+                        id="tarefa"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({...this.state, tarefa: evento.target.value })}
+                        placeholder="O que você quer estudar?" 
+                        required 
+                    />
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor="tempo">Tempo</label>
-                    <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required />
+                    <input 
+                        type="time" 
+                        step="1" 
+                        name="tempo" 
+                        value={this.state.tempo} 
+                        onChange={evento => this.setState({...this.state, tempo: evento.target.value })}
+                        id="tempo" min="00:00:00" 
+                        max="01:30:00" required 
+                    />
                 </div>
-                <Button> Adicionar </Button>
+                <Button type="submit"> Adicionar </Button>
             </form>
         );
     }
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from './button.module.scss';
 
-class Button extends React.Component {
+class Button extends React.Component<{
+  type?: "button" | "submit" | "reset" | undefined
+}> {
   render() {
+    const { type = "button"} = this.props;
     return (
-      <button className={style.botao}>
+      <button className={style.botao} type={type}>
         {this.props.children}
       </button>
     );
